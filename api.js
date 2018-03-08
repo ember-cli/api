6f2a0782aff4daaf2cc512f566683f2c4feb2065
Travis <noreply@travis-ci.org>
2018-03-08 10:12:15 +0000

    Deploy to GitHub Pages

---
+++ api.js
@@ -0,0 +1,36 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Addon",
+        "AddonDiscovery",
+        "AddonsFactory",
+        "AmdTransformAddon",
+        "Assembler",
+        "Blueprint",
+        "Builder",
+        "CLI",
+        "Command",
+        "ConcatenationStrategy",
+        "EmberAddon",
+        "EmberApp",
+        "HistorySupportAddon",
+        "Instrumentation",
+        "NpmTask",
+        "Project",
+        "ServeFilesAddon",
+        "TestsServerAddon",
+        "WatcherAddon",
+        "WindowsSymlinkChecker"
+    ],
+    "modules": [
+        "ember-cli"
+    ],
+    "allModules": [
+        {
+            "displayName": "ember-cli",
+            "name": "ember-cli"
+        }
+    ],
+    "elements": []
+} };
+});
